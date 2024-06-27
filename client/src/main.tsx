542893af595b08f3968854410d55bb12e358ae6f
PLThabangR <rakgoropthabang@gmail.com>
2024-06-27 23:21:38 +0200

    Done with the setup of CLerk in the main.tsx file using public key

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {ClerkProvider} from "@clerk/clerk-react"
 
+//Import the .env variable
+const PUBLSHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+//If no key do not run the app
+if(!PUBLSHABLE_KEY){
+  throw new Error("Missing Publishable key")
+}
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLSHABLE_KEY}>
     <App />
+    </ClerkProvider>
+   
   </React.StrictMode>,
 )
