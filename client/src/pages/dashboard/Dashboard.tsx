ef5558ffcd50d70bb5f01956c82f629a6885162a
PLThabangR <rakgoropthabang@gmail.com>
2024-06-28 01:11:01 +0200

    Using Clerk hooks to get user details

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,13 @@
-
+import {useUser} from "@clerk/clerk-react"
 
 const Dashboard = () => {
+    //Access user details using one of the clerks hooks
+    const {user} = useUser()
   return (
-    <div>Dashboard</div>
+    <div className="dashboard-container">
+        <div className="div"><h1>Welcome {user?.firstName}! here are your finances</h1></div>
+        <div className="div"></div>
+    </div>
   )
 }
 
