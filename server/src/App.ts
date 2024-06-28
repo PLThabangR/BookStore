bd9f34b6089d10818a0c526f2b33cf6a1b6f3872
PLThabangR <rakgoropthabang@gmail.com>
2024-06-28 03:42:33 +0200

    Server connecting to the MongoDB

---
+++ server/src/App.ts
@@ -0,0 +1,19 @@
+import express,{Express} from "express";
+import mongoose from "mongoose";
+
+//Instace of express
+const app : Express = express()
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(express.json())
+
+//MongoDB connection using vsCode url
+const  mongoURl :string = "mongodb+srv://thabang:thabang@finance.0snlwxc.mongodb.net/"
+
+// Using VsCode url to connect with mongo DB
+//mongoose.connect(mongoURl).then(()=> console.log("Connected to MONGODB")).catch((err)=>console.log("Cannot connect to MONGODB",err))
+
+app.listen(port,()=>{
+    console.log(`Server running on por ${port}`)
+})
---
+++ server/src/Model/FinancialRecord.ts
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+//Schema
+const financeRecoordSchema= new mongoose.Schema({
+    
+})
