15f7f7384449c3a15659b6f9fc041fa665f14c3c
PLThabangR <rakgoropothabang@gmail.com>
2024-06-28 03:47:19 +0200

    Added interface in the express

--- server/src/Model/FinancialRecord.ts
+++ server/src/Model/FinancialRecord.ts
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
+//Create userRecord interface
+interface FinancialRecord{
+    userId:string;
+    date:Date;
+    description:string;
+    category:string;
+    paymentMethod:string;
+}
 
 //Schema
 const financeRecoordSchema= new mongoose.Schema({
-    
+
 })
